c2eb0e1a82f803dac88d44ed7f0b317310427f67
Torrey Smith <tas.smity@yahoo.com>
2022-10-12 07:23:31 -0700

    changed page titles for google analytics

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.rustwipes.com/:path*',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
