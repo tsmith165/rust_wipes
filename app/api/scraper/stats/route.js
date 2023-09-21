5dee47fb9fdd855e171eaa42e00ea3e3b126bcc0
Torrey Smith <tas.smity@yahoo.com>
2023-09-21 06:32:32 -0700

    Implementing changes required for Next 13.5.1 with App Directory enabled

--- app/api/scraper/stats/route.js
+++ app/api/scraper/stats/route.js
@@ -1,18 +1,22 @@
 import { prisma } from "../../../../lib/prisma";
 
 export async function POST(req, res) {
-    const { page = 1, itemsPerPage = 5 } = req.query;
-    const skip = (page - 1) * itemsPerPage;
+    try {
+        const { page = 1, itemsPerPage = 5 } = req.query;
+        const skip = (page - 1) * itemsPerPage;
 
-    const stats = await prisma.scraper_stats.findMany({
-        orderBy: { date: "desc" },
-        skip,
-        take: itemsPerPage,
-    });
+        const stats = await prisma.scraper_stats.findMany({
+            orderBy: { date: "desc" },
+            skip,
+            take: itemsPerPage,
+        });
 
-    const totalStatsCount = await prisma.scraper_stats.count();
+        const totalStatsCount = await prisma.scraper_stats.count();
 
-    const totalPages = Math.ceil(totalStatsCount / itemsPerPage);
+        const totalPages = Math.ceil(totalStatsCount / itemsPerPage);
 
-    res.status(200).json({ stats, totalPages });
+        return res.status(200).json({ stats, totalPages });
+    } catch (error) {
+        return res.status(500).json({ error: 'An error occurred.' });
+    }
 }
