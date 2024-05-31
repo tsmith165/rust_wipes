df8ef738509f66cf14c4621a4062ed8a2405fe9d
Torrey Smith <tas.smity@yahoo.com>
2024-05-31 05:27:30 -0700

    Removing prisma

--- prisma/migrations/20221001161301_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Server" (
-    "id" SERIAL NOT NULL,
-    "bm_id" INTEGER NOT NULL,
-    "title" TEXT NOT NULL,
-    "wipes" TEXT[],
-
-    CONSTRAINT "Server_pkey" PRIMARY KEY ("id")
-);
