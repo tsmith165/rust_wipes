be43cd7cd1efbdb522941b25198633c4bd291aba
Torrey Smith <tas.smity@yahoo.com>
2024-06-01 04:39:38 -0700

    Moved all page component files into their respecitive app dir

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+import '@/styles/globals/globals.scss';
+
+interface RootLayoutProps {
+    children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+    console.log(`Loading Root Layout...`);
+
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
