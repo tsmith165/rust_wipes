415c3d8264302b848f748ca04c89b7236facdb30
tsmith165 <torreysmith165@gmail.com>
2024-06-02 17:43:32 -0700

    Updated menu_list typescript

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { menu_list, MenuItem } from '@/lib/menu_list';
+import { menu_list, MenuItem } from '@/lib/menu_list.js';
 
 interface NavbarProps {
     page: string;
