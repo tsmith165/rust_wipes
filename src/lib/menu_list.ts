02a69a08f27b39678e8214bcc21833d463e338fd
Torrey Smith <tas.smity@yahoo.com>
2024-09-07 13:55:10 -0700

    Updated styling for gambling wheel

--- src/lib/menu_list.ts
+++ src/lib/menu_list.ts
@@ -3,7 +3,7 @@ import PROJECT_CONSTANTS from '@/lib/constants';
 export const navbar_menu_list: [string, string, string][] = [
     ['servers', 'Servers', '/servers'],
     ['kits', 'Kits', '/kits?type=monthly&kit=2'],
-    ['gambling', 'Gambling', '/gambling/wheel'],
+    ['wheel', 'Wheel', '/gambling/wheel'],
     ['stats', 'Stats', '/stats'],
     ['networks', 'Networks', '/networks'],
     ['recent', 'Recent', '/recent?page=1&numServers=25&minPlayers=2&maxDist=5000&country=US'],
