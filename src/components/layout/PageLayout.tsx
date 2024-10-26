88c1f551da1df7d3675e56b2eb477bff1c95b61e
Torrey Smith <tas.smity@yahoo.com>
2024-10-26 07:45:29 -0700

    Updated upcoming page to use nuqs

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import Navbar from './Navbar';
+import { Suspense } from 'react';
 import PostHogPageView from '@/app/PostHogPageView';
 
 type PageLayoutProps = {
@@ -10,7 +11,9 @@ type PageLayoutProps = {
 export default function PageLayout({ children, page }: PageLayoutProps) {
     return (
         <div className="h-[100dvh] bg-stone-900">
-            <PostHogPageView />
+            <Suspense>
+                <PostHogPageView />
+            </Suspense>
             <Navbar page={page} />
             <main className="h-[calc(100dvh-50px)]">{children}</main>
         </div>
